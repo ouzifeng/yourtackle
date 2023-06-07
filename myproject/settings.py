4f9579116bb7e36e72ba29bd812a5b1cfe3f1f97
David Oak <mr.davidoak@gmail.com>
2023-06-07 14:57:35 +0100

    sql db

--- myproject/settings.py
+++ myproject/settings.py
@@ -90,7 +90,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 ROOT_URLCONF = "myproject.urls"
 
