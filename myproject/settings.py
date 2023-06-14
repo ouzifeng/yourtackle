e770c1307427928dd48d60b3437bd70c1113f64a
David Oak <mr.davidoak@gmail.com>
2023-06-14 13:42:36 +0100

    neon postgres

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,11 +132,16 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'ouzifeng',
+    'PASSWORD': '3yWFlSJ0RhOj',
+    'HOST': 'ep-shiny-flower-350995.eu-central-1.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
 
 
