24fd0e3ecdfb41cbe8d45421850ac1381913cad7
David Oak <mr.davidoak@gmail.com>
2023-07-10 11:06:43 +0100

    change back to sqllite

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,12 +135,8 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'neondb',
-    'USER': os.environ.get('DB_USER'),
-    'PASSWORD': os.environ.get('DB_PASSWORD'),
-    'HOST': 'ep-shiny-flower-350995.eu-central-1.aws.neon.tech',
-    'PORT': '5432',
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
   }
 }
 
